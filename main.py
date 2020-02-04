f2f27ac2c8ca54b4d77c25965efba3c92ada436a
Steven <60404094+sltayl19@users.noreply.github.com>
2020-02-04 14:33:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 # A starter program for Python with Tkinter
 
 from tkinter import * # import Tkinter library
-window = Tk()         # Create the application window
+window = Tk()       # Create the application window
+
+window.geometry('350x200') # set window size
 
 # Add a label with the text "Hello"
-lbl = Label(window, text="Hello Roberto")
-
-# Place the label in the window at 0, 0
+lbl = Label(window, text="Hello")
 lbl.grid(column=0, row=0)
 
+big = Label(window, text="Hello", font=("Arial Bold", 50))
+big.grid(column=0,row=3)
 
-window.mainloop()     # Keep the window open
+l1 = Label(window, text="h")
+l1.grid(column=0, row=1)
+
+
+txt = Entry(window,width=10)
+txt.grid(column=1, row=0)
+
